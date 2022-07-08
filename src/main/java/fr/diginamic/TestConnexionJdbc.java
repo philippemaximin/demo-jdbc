f8757a11ab4b93022c4eae74848e3c5751c4d59e
Philippe Maximin <philippe_maximin@yahoo.fr>
2022-07-08 15:45:42 +0200

    tp2

--- src/main/java/fr/diginamic/TestConnexionJdbc.java
+++ src/main/java/fr/diginamic/TestConnexionJdbc.java
@@ -4,20 +4,37 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class TestConnexionJdbc {
 
+	
+	private static final String URL;
+	private static final String USER;
+	private static final String PWD;
+	private static final String PORT;
+	
+	static {
+		ResourceBundle res = ResourceBundle.getBundle("db");
+		URL = res.getString("url");
+		USER = res.getString("user");
+		PWD = res.getString("pwd");
+		PORT = res.getString("port");
+	}
+	
+	
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		
-		String url = "jdbc:mariadb://localhost/compta2";
 		Properties props = new Properties();
-		props.setProperty("user","ezechiel");
-		props.setProperty("password","fukijada");
+		props.setProperty("user", USER);
+		props.setProperty("password", PWD);
+		props.setProperty("port", PORT);
 		
-		Connection conn = DriverManager.getConnection(url, props);
+		Connection conn = DriverManager.getConnection(URL, props);
 
 		System.out.println(conn);
+
 	}
 
 }
